3285272ed93d3a4be07c0fc404aed5167f9a9bd7
ChiChaChai <chatchaishaetan@gmail.com>
2017-08-21 10:54:33 +0700

    recreate 1_5_2_1

--- 1_5_2_1.py
+++ 1_5_2_1.py
@@ -1,6 +1,15 @@
+import numpy as np
+
 def laff_copy(x,y):
-    for i in range(len(y)):
-        try:
-            x[i] = y[i]
-        except:
-            print('FAILED')
+    # for i in range(len(y)):
+    #     try:
+    #         x[i] = y[i]
+    #     except:
+    #         print('FAILED')
+    if x.shape != y.shape:
+        print('FAILED')
+        return
+    x = y.copy()
+    print(x)
+
+laff_copy(np.matrix('1 2 3;4 5 6'),np.matrix('5 2 5;4 5 1'))
